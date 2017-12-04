83d3cacac0b8102815a5f4d1ba2aff887d64711f
eriansyah20 <eriansyah20@gmail.com>
2017-12-04 09:15:23 +0700

    Report Sto

---
+++ application/APP_CI/modules/ReportSto/controllers/Rassetdata.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Rassetdata extends MX_Controller {
+
+    function __construct() {
+        
+    }
+        public function index (){
+            $this->load->view('V_rassetdata');
+        }
+        public function read(){
+        $this->load->model('R_rassetdata');
+        $start = $this->input->post('start');
+        $limit = $this->input->post('limit');
+        $filter = array();
+        $filter = $this->input->post('filter');
+        header('Content-type: application/json');
+        print_r( $this->R_rassetdata->load_default($start,$limit,$filter));
+        
+    }
+    public function exportAsset(){
+        $this->load->model('Rreportasset');
+        $data['query'] = $this->Rreportasset->load_default();
+        $this->load->view('vexcel', $data);
+    }
+
+}
